58d29de98d181e6591c1a949e815298c0e808b74
z0l1 <116083393+IamDaddyZ0l1@users.noreply.github.com>
2022-11-08 13:26:36 +0100

    Update 5. program.py

--- 5. program.py
+++ 5. program.py
@@ -1,9 +1,14 @@
-print("Ez egy számológép súlyemelők számára!")
+print("Üdvözöllek a súlyemelők számológépében!")
 
-rud = float(input("Add meg kg-ban a súlyát annak a rúdnak, amit használni fogsz: ")) 
+sets = int(input("Mennyi ismétlésed lesz? "))
 
-suly = float(input("Add meg kg-ban azt a súlymennyiséget, amit rá szeretnél rakni a rúdra: "))
+for x in range(0,sets,1):
+  rud = float(input("Add meg kg-ban a súlyát annak a rúdnak, amit használni fogsz: ")) 
 
-rud_es_suly = rud + suly
+  suly = float(input("Add meg kg-ban azt a súlymennyiséget, amit rá szeretnél rakni a rúdra: "))
 
-print("Ez összesen ", rud_es_suly, "kg")
+  rud_es_suly = rud + suly
+
+  print("Ez összesen ", rud_es_suly, "kg")
+ 
+print("Tartsd fent a kemyén munkát, találkoznk a következő edzésnél!")
