8af25bf7deffef0e435d4d1fbdbc32532a63d0c7
z0l1 <116083393+IamDaddyZ0l1@users.noreply.github.com>
2022-11-08 13:18:06 +0100

    Update 4. program.py

--- 4. program.py
+++ 4. program.py
@@ -7,3 +7,15 @@ szulev = int(input("Add meg a születési évedet: "))
 kor = ev - szulev
 
 print(kor, "éves vagy!")
+
+felnott = 18
+
+
+if kor > felnott:
+  print("Már felnőtt vagy!")
+  
+if kor < felnott:
+  print("Még gyerek vagy!")
+  
+if kor == felnott:
+  print("Még nemrég lettél felnőtt!")
